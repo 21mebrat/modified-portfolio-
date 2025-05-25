cf989fe848488890dd212d64432d4886ec1adb4e
Mebrat <129731218+21mebrat@users.noreply.github.com>
2025-05-25 16:24:58 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full px-3  md:w-1/2">
+        <div className="text-center flex flex-col justify-center xl:pt-40 text-left h-full px-3  md:w-1/2">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
