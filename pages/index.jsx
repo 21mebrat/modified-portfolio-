453a0f9011831217ae3492cec74707c261a12728
Mebrat <129731218+21mebrat@users.noreply.github.com>
2025-05-25 11:51:26 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,8 +21,8 @@ const Home = () => {
             exit="hidden"
             className="h1 sm:hidden xl:flex xl:max-w-none pointer-events-none select-none"
           >
-            Hi, I'm <br /> Mebrat {" "}
-            <span className="text-green-400">Matebie</span>
+            Hi, I'm <br /> Matebie {" "}
+            <span className="text-green-400">Mebrat</span>
           </motion.h1>
           <h2 className="text-2xl md:text-3xl text-white">
             I’m a&nbsp;
